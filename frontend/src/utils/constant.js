90d08536536d98165fe798d00874a763f110c3a0
Shivansh Kumar Jha <kumarjhashivansh@gmail.com>
2025-06-01 15:55:54 +0530

    Updated the frontend codebase

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,4 +1,14 @@
-export const USER_API_END_POINT="http://localhost:8000/api/v1/user";
-export const JOB_API_END_POINT="http://localhost:8000/api/v1/job";
-export const APPLICATION_API_END_POINT="http://localhost:8000/api/v1/application";
-export const COMPANY_API_END_POINT="http://localhost:8000/api/v1/company";
+const localhost = true;
+
+export const USER_API_END_POINT = `http://${
+  localhost ? 'localhost:5001' : ''
+}/api/v1/user`;
+export const JOB_API_END_POINT = `http://${
+  localhost ? 'localhost:5001' : ''
+}/api/v1/job`;
+export const APPLICATION_API_END_POINT = `http://${
+  localhost ? 'localhost:5001' : ''
+}/api/v1/application`;
+export const COMPANY_API_END_POINT = `http://${
+  localhost ? 'localhost:5001' : ''
+}/api/v1/company`;
